b5024c0c587a02f720c8af5f7cd108bb12913fd0
Yevgeniy Ustinov <yustinov@c3r7s1.42prague.com>
2024-09-26 14:38:06 +0200

    Working with easy prints also detecting format issues

--- main.c
+++ main.c
@@ -6,12 +6,16 @@
 /*   By: yustinov <ev.ustinov03@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:19:11 by yustinov          #+#    #+#             */
-/*   Updated: 2024/09/26 14:27:27 by yustinov         ###   ########.fr       */
+/*   Updated: 2024/09/26 14:37:31 by yustinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 int main(void)
 {
-	ft_printf("Hello, from custom printf!");
+	ft_printf("Hello, from custom printf!\n");
+	ft_printf("Trying to print out char 'c':  %c\n", 'c');
+	ft_printf("Trying to print out a string 'Hello': %s\n", "Hello");
+	/*ft_printf("Testing compile time protections", 1, 2); Detecting*/
+	ft_printf("Trying to print i, d types: %d", 42);
 }
