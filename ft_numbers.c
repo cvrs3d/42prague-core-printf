42196cd82f76599d9ab01c781d4757b14002b327
Yevgeniy Ustinov <yustinov@c3r7s1.42prague.com>
2024-09-26 14:25:50 +0200

    Added helper functions definitions

---
+++ ft_numbers.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_numbers.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yustinov <ev.ustinov03@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 13:23:57 by yustinov          #+#    #+#             */
+/*   Updated: 2024/09/26 13:58:12 by yustinov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+/*
+ * Simple numlen function
+ * that calculates number
+ * of digits in the   int
+ */
+int	ft_numlen(int number)
+{
+	int		len;
+	long	n;
+
+	len = 0;
+	n = (long)number;
+	if (n < 0)
+		n *= -1;
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
---
+++ ft_strings.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strings.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yustinov <ev.ustinov03@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 13:37:38 by yustinov          #+#    #+#             */
+/*   Updated: 2024/09/26 14:18:24 by yustinov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+/*
+ * Computes string length, while this
+ * is internaly defined function    I
+ * do not think that NULL checking is
+ * very useful here, because  pointer
+ * is checked before so it kind    of
+ * safe?...Anyways added.
+*/
+int	ft_strlen(const char *string)
+{
+	int	len;
+
+	if (string == NULL)
+		return (0);
+	len = 0;
+	while (*string != '\0')
+	{
+		string++;
+		len++;
+	}
+	return (len);
+}
+
+int	ft_is_alpha(int	chr)
+{
+	if (chr >= 'a' && chr <= 'z')
+		return (1);
+	if (chr >= 'A' && chr <= 'Z')
+		return (1);
+	return (0);
+}
+
+int	ft_isdigit(int chr)
+{
+	if (chr >= '0' && chr <= '9')
+		return (1);
+	return (0);
+}
